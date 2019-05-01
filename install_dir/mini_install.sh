cbedf95aa3c577b9da2383442a0045e37c89581a
starqingzhu <starqingzhu@163.com>
2019-05-02 01:37:26 +0800

    刚装完系统,最小化安装脚本

---
+++ install_dir/mini_install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#需要每次修改该主机名
+CURHOSTNAME="mac_home1"
+#查看本机ip
+echo "ip 地址:---------------------------------------->"
+ip addr 
+
+#找出那些包提供了
+echo "安装ifconfig相关-------------------------------->"
+yum whatprovides ifconfig
+yum install net-tools
+
+sleep 1
+#安装编辑及编译工具
+echo "安装gcc g++相关--------------------------------->"
+yum -y install vim gcc gcc-c++ g++  lrzsz  unzip zip wget cmake git ntp ntpdate telnet mailx
+
+#参照修改静态ip文档
+#修改hostname
+#echo $HOSTNAME
+echo "修改hostname相关-------------------------------->"
+cat  /etc/hostname
+echo $CURHOSTNAME > /etc/hostname
+cat  /etc/hostname
+
+#更新安装已有的软件最新版本以及安全升级
+echo "更新系统软件相关-------------------------------->"
+yum update && yum upgrade
+
+#自动更新和升级
+yum -y update && yum -y upgrade
+
+#安装命令行web浏览器
+echo "安装web浏览器相关-------------------------------->"
+yum install links
+
+#关闭防火墙
+echo "关闭防火墙--------------------------------------->"
+systemctl disable firewalld.service
+#安装 Apache HTTP服务器
+echo "安装Apache http服务器---------------------------->"
+yum install httpd
+systemctl restart httpd.service
+systemctl enable httpd.service
+
+#测试links
+links 127.0.0.1
---
+++ install_dir/re_ip.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+#新的机器执行需要修改的字段 NAME  DEVICE IPADDR GATEWAY DNS1 DNS2
+#查看当前机器的语言字符集
+cat /etc/locale.conf
+
+#查看当前ip
+ip addr
+
+#修改/etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'TYPE="Ethernet"' > /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'PROXY_METHOD="none"' > /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'BROWSER_ONLY="no"' > /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'BOOTPROTO="static"' >> /etc/sysconfig/network-scripts/ifcfg-eth0 #dhcp为动态  static为静态 ip
+echo 'DEFROUTE="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'PEERDNS="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'PEERROUTES="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV4_FAILURE_FATAL="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6INIT="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6_AUTOCONF="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6_DEFROUTE="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6_PEERDNS="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6_PEERROUTES="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6_FAILURE_FATAL="no"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'IPV6_ADDR_GEN_MODE="stable-privacy"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'NAME="eth0"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo '#UUID="4bcf16f5-ae8b-4a38-898d-da32f34c3ca0"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'DEVICE="eth0"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo 'ONBOOT="yes"' >> /etc/sysconfig/network-scripts/ifcfg-eth0 #开机启用本配置
+echo 'NM_CONTROLLED=no' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+
+echo 'IPADDR=192.168.2.121' >> /etc/sysconfig/network-scripts/ifcfg-eth0   #静态ip
+echo 'GATEWAY=192.168.2.1' >> /etc/sysconfig/network-scripts/ifcfg-eth0    #默认网关
+echo 'NETMASK=255.255.255.0' >> /etc/sysconfig/network-scripts/ifcfg-eth0  #子网掩码
+echo 'DNS1=192.168.2.1' >> /etc/sysconfig/network-scripts/ifcfg-eth0      #首选dns服务器
+
+#重启
+systemctl restart network
